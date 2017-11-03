3a5fee2aad02f31ccddf355ea0b842b48a8e7343
Sean Moon <gzson79@gmail.com>
2017-11-03 18:38:55 -0400

    integrated mongo db

--- fuzzyserver/src/test/kotlin/com/devtechnology/coi/fuzzy/HelloTest.kt
+++ fuzzyserver/src/test/kotlin/com/devtechnology/coi/fuzzy/HelloTest.kt
@@ -1,11 +1,15 @@
 package com.devtechnology.coi.fuzzy
 
-import getHelloString
-import kotlin.test.assertEquals
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class HelloTest {
     @Test fun testAssert() : Unit {
         assertEquals("Hello World", getHelloString())
+    }
+
+    @Test fun mongo_return_more_than_one_hello() : Unit {
+        assertTrue { getAllHellosFromMongo() != null }
     }
 }
