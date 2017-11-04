b7de395e13f883281ed7f0a5ea88caf1cf08c438
Zach Melnick <zachary.melnick@gmail.com>
2017-11-03 21:42:19 -0400

    Added web method to insert new items into the database

--- fuzzyserver/src/main/kotlin/com/devtechnology/coi/fuzzy/Main.kt
+++ fuzzyserver/src/main/kotlin/com/devtechnology/coi/fuzzy/Main.kt
@@ -1,7 +1,7 @@
 package com.devtechnology.coi.fuzzy
 
+import com.devtechnology.coi.fuzzy.data.Hello
 import com.devtechnology.coi.fuzzy.data.MongoConnector
-import com.mongodb.Block
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -11,7 +11,8 @@ import io.ktor.routing.routing
 import io.ktor.server.host.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.util.toMap
-import org.litote.kmongo.KMongo
+import org.litote.kmongo.getCollection
+import org.litote.kmongo.save
 
 
 fun main(args: Array<String>) {
@@ -25,6 +26,12 @@ fun main(args: Array<String>) {
                 call.request.queryParameters.forEach { s, list -> sb.append(s).append(list) }
                 call.respondText ( sb.toString(), ContentType.Text.Html )
             }
+            post(path = "/putHello") {
+                val entries: List<Hello> = call.request.queryParameters.toMap()
+                        .map { entry -> Hello(name = entry.key, text = entry.value[0]) }
+                entries.forEach(insertHello(it))
+
+            }
         }
     }
     server.start(wait = true)
@@ -36,4 +43,8 @@ fun getAllHellosFromMongo() : String {
     val sb = StringBuilder()
     MongoConnector.getCollection("hello").find().forEach { sb.append(" ${it.text} by ${it.by} @ ${it.date}") }
     return sb.toString()
+}
+
+fun insertHello(newHello: Hello) {
+    MongoConnector.database.getCollection<Hello>().save(newHello)
 }
